6666342df4e613396d294153e5060f7ed55c01f0
Kiran Vaddi <kiranvad@cpn-f07-26.cbls.ccr.buffalo.edu>
2020-01-09 17:28:35 -0500

    update homcycles with neater computation

--- helpers/homcycles.py
+++ helpers/homcycles.py
@@ -2,69 +2,139 @@
 This is a helper function to compute representative cycles using dionysus.
 This function has the following functions implemented:
 
-    findConnectedComponents : Which determines vertices in any particular bar of zero-dimension
-    FindRepresentativeCycles: Which determines representative cycles of any particular bar
+    FindRepresentativeCycles   : Which determines representative cycles of any particular bar
+    ComputeConnectedComponents : Computes connected components for each bar above a specified threshold
+    from_hfp_to_connectedcomps : Computes connected components of a given bar
     
 (c) Copyright Kiran Vaddi 01-2020
 
 '''
-import dionysus as dion
 import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import connected_components
+import networkx as nx
+import time
 
-def findConnectedComponents(start_index):
-    '''
-    A helper function to compute connected component vertices.
-    This function takes index of any filtration where a 0-cycle death is detected and
-    returns all the vetices that are in the bar corresponding to the 0-cycle that was dead.
-    '''
-    nodes = list(filtration[start_index])
-    pairs = []
-    vertices = []
-    while not any(p == hfp.unpaired for p in pairs):
-        nodes_temp = []
-        vertices.append(nodes)
-        for n in nodes:
-            # print(n, hfp.pair(n))
-            pairs.append(hfp.pair(n))
-            if hfp.pair(n) != hfp.unpaired:
-                nodes_temp.append(list(filtration[hfp.pair(n)]))
-        nodes_temp = np.unique(np.asarray(nodes_temp))
-        nodes = np.setdiff1d(nodes_temp, nodes).tolist()
-    vertices = np.unique(np.hstack(vertices))
-    # print(vertices)
-    return vertices
+__all__ = ["from_hfp_to_connectedcomps", "FindRepresentativeCycles", "ComputeConnectedComponents"]
 
-def FindRepresentativeCycles(HomPer, thresh=0.0):
-    '''
-    This function can be used to collect representative cycles of the bars in a persistence
-    diagram.
-    This function takes a homology persistence class from dionysus as a input along with
-    an optional threshold value in terms of the lifetime of the bar.
-    The output is a numpy array of representative cycles. Each row in the array is one cycle per bar,
-    with last three columns of each row reprsenting: dimension, birth and death of the cycle.
-    Rest of the columns in the array are vertices in the cycle.
+class homcycles(object):
     
-    All the dimensional cycles in the homology persistence are computed by default.
-    '''
-    repcycles = []
-    ccverts = []
-    for i,c in enumerate(hfp):
-        if len(c)>0:
-            birth = filtration[i].data 
-            dim = filtration[i].dimension()-1
-            deaths =[]
-            vertices = []
-            for x in c:
-                simplex = filtration[x.index]
-                deaths.append(filtration[x.index].data)
-                for s in simplex:
-                    vertices.append(s)
-            death = np.max(np.unique(np.asarray(deaths)))
-            vertices = np.unique(np.asarray(vertices))
-            vertices = np.append(vertices,[dim, birth, death])
-            if birth-death>thresh:
-                repcycles.append(vertices)
+    def __init__(self, hfp, filtration, num_vertices, thresh=0.0):
+        """
+        A class for various operations using homology cycles:
+        
+        Parameters:
+        -----------
+                hfp : Homology peristenece class from dionysus2
+                filtration : filtration from dionysus2
+                num_vertices : number of 0-dimensional simplices in the data (typically number of data points)
+                thresh : death threshold for computation
+        """
+        self.thresh = thresh
+        self.hfp = hfp
+        self.filtration = filtration
+        self.num_vertices = num_vertices
+
+    def FindRepresentativeCycles(self):
+        '''
+        This function can be used to collect representative cycles of the bars in a persistence
+        diagram.
+        This function takes a homology persistence class from dionysus as a input along with
+        an optional threshold value in terms of the lifetime of the bar.
+        The output is a numpy array of representative cycles. Each row in the array is one cycle per bar,
+        with last three columns of each row reprsenting: dimension, birth and death of the cycle.
+        Rest of the columns in the array are vertices in the cycle.
+
+        All the dimensional cycles in the homology persistence are computed by default.
+        '''
+        
+        repcycles = []
+        ccverts = []
+        for i,c in enumerate(self.hfp):
+            if len(c)>0:
+                birth = self.filtration[i].data 
+                dim = self.filtration[i].dimension()-1
+                deaths =[]
+                vertices = []
+                for x in c:
+                    simplex = self.filtration[x.index]
+                    deaths.append(self.filtration[x.index].data)
+                    for s in simplex:
+                        vertices.append(s)
+                death = np.max(np.unique(np.asarray(deaths)))
+                vertices = np.unique(np.asarray(vertices))
+                vertices = np.append(vertices,[dim, birth, death])
+                if birth-death>self.thresh:
+                    repcycles.append(vertices)
+        return repcycles
+    
+    def from_hfp_to_connectedcomps(self,index):
+        """
+        Computes connected components upto a death of a 0-cycle.
+        Parameters:
+        -----------
+                hfp : Homology peristenece class from dionysus2
+                filtration : filtration from dionysus2
+                num_vertices : number of 0-dimensional simplices in the data (typically number of data points)
+                index : filtration index of death of 0-cycle
+
+        Returns:
+        --------
+        List of connected components as a set
+        """
+        total_iters = np.arange(self.num_vertices,index+1)
+        adj = np.eye(self.num_vertices)
+        for i,doi in enumerate(total_iters):
+            current_vertices = list(self.filtration[doi])
+            if doi==index:
+                repvertex = current_vertices[1]
+            adj[current_vertices[0],current_vertices[1]] = 1;
+        adj = np.maximum( adj, adj.T)
+        graph = csr_matrix(adj)
+        n_components, labels = connected_components(csgraph=graph, directed=False, return_labels=True)
+        G = nx.from_scipy_sparse_matrix(graph)
+        concomps = nx.node_connected_component(G, repvertex)
+        # print('Vertex %d is connected to'%(repvertex),concomps)
+        return concomps
+
+    def ComputeConnectedComponents(self):
+        """
+        Computes connected components of bars above a threshold.
+        Parameters:
+        -----------
+                hfp          : Homology peristenece class from dionysus2
+                filtration   : filtration from dionysus2
+                num_vertices : number of 0-dimensional simplices in the data (typically number of data points)
+                thresh       : Threshold of death time of bars (default, 0.0)
+
+        Returns:
+        --------
+        List of connected components as a dictornary, one for each bar. Dictornary has following keys:
+                'cc'    : connected conponents as a list
+                'birth' : birth of the bar
+                'death' : death of the bar
+                'index' : Index into the filtration where the bar is ended (birth of its representative 1-cycles)
+        """
+        all_concomps = []
+        for i,c in enumerate(self.hfp):
+            if len(c)>0:
+                dim = self.filtration[i].dimension()-1
                 if dim==0:
-                    ccverts.append(findConnectedComponents(i))                
-    return repcycles, ccverts
-    
+                    death = self.filtration[i].data 
+                    births =[]
+                    for x in c:
+                        simplex = self.filtration[x.index]
+                        births.append(self.filtration[x.index].data)
+                    birth = np.max(np.unique(np.asarray(births)))
+                    if death-birth>self.thresh:
+                        print('Computing Bar %d which was killed at %0.3f'%(i,death))
+                        start = time.time()
+                        concomps={}
+                        concomps['cc'] = self.from_hfp_to_connectedcomps(i)
+                        concomps['birth'] = birth
+                        concomps['death'] = death
+                        concomps['index'] = i
+                        all_concomps.append(concomps.copy())
+                        end = time.time()
+                        print('Elapsed Time is : ',end-start)
+        return all_concomps
